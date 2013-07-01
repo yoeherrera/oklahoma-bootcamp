6d2f949862a78504c8b6adea85ad39188b276ce6
yoe herrera <yoeherrera@gmail.com>
2013-07-01 14:29:46 -0500

    Added module with functions

--- add_numb.py
+++ add_numb.py
@@ -1,11 +1,14 @@
 #This has been done Yoe, hope it will get me a job someday in the future.
 
+from adding import add_two, add_three
+
 def add_two(a,b):
 return a+b
 
 def main():
+print ("2+2+2=", add_three(2,2,2))
 print ("2+3= ",add_two(2,3))
 
-if __name__== "__main__"
+if __name__== "__main__":
       main()
 
