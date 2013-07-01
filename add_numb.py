5ce5e5bdfb16568223e200c73e00b4fcfe36bdac
yoe herrera <yoeherrera@gmail.com>
2013-07-01 14:36:53 -0500

    more stuff

--- add_numb.py
+++ add_numb.py
@@ -1,10 +1,12 @@
 #This has been done Yoe, hope it will get me a job someday in the future.
 
 def add_two(a,b):
+   print("Hi, just adding")
 return a+b
 
 def main():
-print ("2+3= ",add_two(2,3))
+   print ("2+3= ",add_two(2,3))
+   print ("4+4=", add_two(4,4))
 
 if __name__== "__main__"
       main()
